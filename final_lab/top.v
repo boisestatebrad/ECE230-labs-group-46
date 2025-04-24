8b4e570fc5a80d2c40d2abf53c0aeaa95f40207e
boisestatebrad <bradames@u.boisestate.edu>
2025-04-24 13:38:23 -0600

    Update top.v

--- final_lab/top.v
+++ final_lab/top.v
@@ -11,12 +11,39 @@ module top
     output [6:0] seg // 7seg segments
 );
   wire x;
+  wire [3:0] anodeout, AplusB_out, AminusB_out;
     
     clock_div one (
         .clock(clk),
         .reset(btnC),
         .div_clock(x)
     );
+    
+    seven_seg_scanner two(
+        .div_clock(x),
+        .reset(btnC),
+        .anode(anodeout)
+    );
+    
+    math_block three(
+        .A(sw[3:0]),
+        .B(sw[7:4]),
+        .AplusB(AplusB_out),
+        .AminusB(AminusB_out)
+    );
+    
+    seven_seg_decoder four(
+        .anode(anodeout),
+        .A(sw[3:0]),
+        .B(sw[7:4]),
+        .AplusB(AplusB_out),
+        .AminusB(AminusB_out),
+        .segs(seg[6:0])
+        );
+       
+       assign anodeout = an[3:0];
+        
+    
     
     // Instantiate the clock divider...
     // ... wire it up to the scanner
