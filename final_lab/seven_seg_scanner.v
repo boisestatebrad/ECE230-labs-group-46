620ffc3de5aa5aaebd43d2a8301baec1b4aaee49
boisestatebrad <bradames@u.boisestate.edu>
2025-04-24 14:25:26 -0600

    Update seven_seg_scanner.v

--- final_lab/seven_seg_scanner.v
+++ final_lab/seven_seg_scanner.v
@@ -1,37 +1,46 @@
 module seven_seg_scanner(
     input div_clock,
     input reset,
-    output reg [3:0] anode
+    output [3:0] anode
 );
 
-    reg [1:0] state;
-    reg [1:0] next_state;
-
-    always @(posedge div_clock or posedge reset) begin
-        if (reset)
-            state <= 2'b00;
-        else
-            state <= next_state;
-    end
-
-    always @(*) begin
-        case (state)
-            2'b00: next_state = 2'b01; 
-            2'b01: next_state = 2'b10; 
-            2'b10: next_state = 2'b11; 
-            2'b11: next_state = 2'b00; 
-            default: next_state = 2'b00;
-        endcase
-    end
-
-    always @(*) begin
-        case (state)
-            2'b00: anode = 4'b1110; 
-            2'b01: anode = 4'b1101; 
-            2'b10: anode = 4'b1011; 
-            2'b11: anode = 4'b0111; 
-            default: anode = 4'b1111;
-        endcase
-    end
+    wire Anext, Bnext, Cnext, Dnext;
+    
+    dff_state Adff (
+    .reset(reset),
+    .clk(div_clock),
+    .D(Anext),
+    .Default(1'b0),
+    .Q(anode[0])
+    );
+    
+    dff_state Bdff (
+    .reset(reset),
+    .clk(div_clock),
+    .D(Bnext),
+    .Default(1'b1),
+    .Q(anode[1])
+    );
+    
+    dff_state Cdff (
+    .reset(reset),
+    .clk(div_clock),
+    .D(Cnext),
+    .Default(1'b1),
+    .Q(anode[2])
+    );
+    
+    dff_state Ddff (
+    .reset(reset),
+    .clk(div_clock),
+    .D(Dnext),
+    .Default(1'b1),
+    .Q(anode[3])
+    );
+        
+    assign Anext = anode[3];
+    assign Bnext = anode[0];
+    assign Cnext = anode[1];
+    assign Dnext = anode[2]; 
 
 endmodule
