0da4e2cd1bab4f58e220c7aa252f07caf212d88a
boisestatebrad <bradames@u.boisestate.edu>
2025-04-24 14:24:48 -0600

    Update math_block.v

--- final_lab/math_block.v
+++ final_lab/math_block.v
@@ -4,72 +4,39 @@ module math_block(
     output [3:0] AplusB,
     output [3:0] AminusB
 );
-    wire [2:0] carry;
+    wire [3:0] negB;
+    wire [3:0] carry_plus;
+    wire [3:0] carry_minus;
     
-    wire [2:0] borrow;
-    
-    full_adder zero_bit(
-        .A(~A[0]),
-        .B(1'b1),
-        .Y(AplusB[0]),
-        .Cout(carry[0]),
-        .Cin(1'b0)
+    twos_compliment eq1(
+    .A(B[3:0]),
+    .out(negB[3:0])
     );
     
-    full_adder one_bit(
-        .A(~A[1]),
-        .B(1'b0),
-        .Y(AplusB[1]),
-        .Cout(carry[1]),
-        .Cin(carry[0])
-    );
+    genvar i;
+    generate
+        for (i = 0; i < 4; i = i + 1) begin
+        wire Cplus;
+        wire Cminus;
+        assign Cadd = (i==0) ? 1'b0 : carry_plus[i - 1]; 
+        assign Cminus = (i==0) ? 1'b0 : carry_minus[i-1];
+            
+            full_adder plus (
+                .A(A[i]),
+                .B(B[i]),
+                .Cin(Cadd),
+                .Cout(carry_plus[i]),
+                .Y(AplusB[i])
+            );
+            
+            full_adder minus (
+                .A(A[i]),
+                .B(negB[i]),
+                .Cin(Cminus),
+                .Cout(carry_minus[i]),
+                .Y(AminusB[i])
+            );
+        end
+    endgenerate 
     
-    full_adder two_bit(
-        .A(~A[2]),
-        .B(1'b0),
-        .Y(AplusB[2]),
-        .Cout(carry[2]),
-        .Cin(carry[1])
-    );
-    
-    full_adder three_bit(
-        .A(~A[3]),
-        .B(1'b0),
-        .Y(AplusB[3]),
-        .Cin(carry[2])
-    );
-   
-    full_subtractor sub_bit0(
-        .A(A[0]),
-        .B(B[0]),
-        .Bin(1'b0),
-        .D(AminusB[0]),
-        .Bout(borrow[0])
-    );
-    
-    full_subtractor sub_bit1(
-        .A(A[1]),
-        .B(B[1]),
-        .Bin(borrow[0]),
-        .D(AminusB[1]),
-        .Bout(borrow[1])
-    );
-    
-    full_subtractor sub_bit2(
-        .A(A[2]),
-        .B(B[2]),
-        .Bin(borrow[1]),
-        .D(AminusB[2]),
-        .Bout(borrow[2])
-    );
-    
-    full_subtractor sub_bit3(
-        .A(A[3]),
-        .B(B[3]),
-        .Bin(borrow[2]),
-        .D(AminusB[3]),
-        .Bout(borrow[3])
-    );
-
-
 endmodule
