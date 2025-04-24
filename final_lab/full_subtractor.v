382cad3f757edcaaaaebafe0b64f36fe40837a7b
boisestatebrad <bradames@u.boisestate.edu>
2025-04-24 14:24:31 -0600

    Update full_subtractor.v

--- final_lab/full_subtractor.v
+++ final_lab/full_subtractor.v
@@ -1,9 +1,1 @@
-module full_subtractor(
-    input A, B, Bin,
-    output D, Bout
-    );
-    
-    assign D = A ^ B ^ Bin;
-    assign Bout = (~A & B) | (~(A ^ B) & Bin);
-    
-endmodule
+// unecessary //
